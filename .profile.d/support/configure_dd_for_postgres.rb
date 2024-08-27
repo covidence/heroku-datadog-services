cdb6f584cfbd50fc6c8f55841d500bb06cc74615
Tommy Fritz <tommy@covidence.org>
2024-08-27 12:46:36 +1200

    Add some new postgres metrics

--- .profile.d/support/configure_dd_for_postgres.rb
+++ .profile.d/support/configure_dd_for_postgres.rb
@@ -23,6 +23,32 @@ ENV.keys.grep(/_URL$/).each do |key|
       'ssl' => true,
       'dbname' => uri.path.split(/^[?\/]/).detect { |p| p && !p.empty? },
       'tags' => tags,
+      'custom_queries' => [{
+        'query' => 'SELECT id FROM public.references ORDER BY id DESC LIMIT 1',
+        'columns' => [
+          { 'name' => 'references_latest_id', 'type' => 'gauge' }
+        ],
+        'collection_interval' => 14_400 # 4 hours
+      },
+      {
+        'query' => 'SELECT id FROM public.study_votes ORDER BY id DESC LIMIT 1',
+        'columns' => [
+          { 'name' => 'study_votes_latest_id', 'type' => 'gauge' }
+        ],
+        'collection_interval' => 14_400 # 4 hours
+      }]
+
+    # custom_queries:
+    #   - query: SELECT foo, COUNT(*) FROM table.events GROUP BY foo
+    #     columns:
+    #     - name: foo
+    #       type: tag
+    #     - name: event.total
+    #       type: gauge
+    #     tags:
+    #     - test:postgresql
+    #     metric_prefix: postgresql
+
     }
   end
 end
