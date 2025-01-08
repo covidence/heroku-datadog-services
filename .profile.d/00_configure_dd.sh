b3977751731ee17adaf348c10e8a7d443cefdab3
Tommy Fritz <tommy@covidence.org>
2025-01-08 15:40:00 +1300

    Move .profile.d to .profile to happen after buildpacks

--- .profile.d/00_configure_dd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ruby .profile.d/support/configure_dd_for_postgres.rb
-ruby .profile.d/support/configure_dd_for_elasticsearch.rb
-ruby .profile.d/support/configure_dd_for_memcache.rb
