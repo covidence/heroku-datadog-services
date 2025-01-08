fc253df31041269424018ab9f64d2a5f60d51587
Tommy Fritz <tommy@covidence.org>
2025-01-08 16:13:49 +1300

    Move start scripts back

--- .profile/00_configure_dd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ruby .profile/support/configure_dd_for_postgres.rb
-ruby .profile/support/configure_dd_for_elasticsearch.rb
-ruby .profile/support/configure_dd_for_memcache.rb
