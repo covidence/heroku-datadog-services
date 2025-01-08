97a4c6957100892139fd0f171789332082ecb309
Tommy Fritz <tommy@covidence.org>
2025-01-08 16:15:55 +1300

    fix

--- .profile.d/00_configure_dd.sh
+++ .profile.d/00_configure_dd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ruby profile.d/support/configure_dd_for_postgres.rb
-ruby profile.d/support/configure_dd_for_elasticsearch.rb
-ruby profile.d/support/configure_dd_for_memcache.rb
+ruby .profile.d/support/configure_dd_for_postgres.rb
+ruby .profile.d/support/configure_dd_for_elasticsearch.rb
+ruby .profile.d/support/configure_dd_for_memcache.rb
